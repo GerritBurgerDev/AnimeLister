b7eb13e8279027500f7ad0ad0f86d4d9b9ca8f4c
ludevinparkinson@gmail.com <41333623+lrparkinson@users.noreply.github.com>
2021-05-17 18:38:24 +0200

    Updated the auth to allow for logout and protected routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import { Router } from '@vaadin/router';
 import './components/index.js';
 
+async function isAuthenticated() {
+  const response = await fetch('http://localhost:1337/authenticate', {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+  });
+  return (await response.text()) === 'authorized';
+}
+
 const routes = [
   {
     path: '/',
     component: 'home-page',
     children: [],
+  },
+  {
+    path: '/anime/:id',
+    component: 'home-page',
+    action: async ctx => {
+      const authenticated = await isAuthenticated();
+      if (!authenticated) {
+        window.location = '/';
+        alert('Please login with your google account');
+      }
+      const { id } = ctx.params;
+      console.log(id);
+    },
   },
 ];
 
