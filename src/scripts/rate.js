b6f168c24f1f135e6d361aa2080c56147595c088
Elex Xabanisa <elex.xabanisa@standardbank.co.za>
2021-05-20 23:06:31 +0200

    Updated anime details to have rating

--- src/scripts/rate.js
+++ src/scripts/rate.js
@@ -1,12 +1,12 @@
 function ratingLevel(level) {
     for (i = 1; i <= 5; i++){
         id = 30+i;
-        document.querySelector("#outlet > home-page").shadowRoot.querySelector("rate-anime").shadowRoot.querySelector("#\\"+id).setAttribute('src', 'assets/images/star.png');
+        document.querySelector("#\\31 5").shadowRoot.querySelector("#\\31 5").shadowRoot.querySelector("div > app-anime-details").shadowRoot.querySelector("div > div.div-layout > rate-anime").shadowRoot.querySelector("#\\"+id).setAttribute('src', 'assets/images/star.png');
     }
     if (level != 0){
         for (i = 1; i <= level; i++){
             id = 30+i;
-            document.querySelector("#outlet > home-page").shadowRoot.querySelector("rate-anime").shadowRoot.querySelector("#\\"+id).setAttribute('src', 'assets/images/checked.png');
+            document.querySelector("#\\31 5").shadowRoot.querySelector("#\\31 5").shadowRoot.querySelector("div > app-anime-details").shadowRoot.querySelector("div > div.div-layout > rate-anime").shadowRoot.querySelector("#\\"+id).setAttribute('src', 'assets/images/checked.png');
         }
     }
 } 
